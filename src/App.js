677679b0df641690205015a97b68aef1f335224b
priyatham <priyatham.cse2016@gmail.com>
2021-03-20 21:53:22 +0530

    bootstrap chnages

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ function App() {
     <div>
 <Header></Header>
 <form>
-  <div class="form-group">
+  <div class="form-group " class="w-25 p-3">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <input type="email" class="form-control form-control-sm "  id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
-  <div class="form-group">
+  <div class="form-group" class="w-25 p-3">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <input type="password" class="form-control form-control-sm"  id="exampleInputPassword1"/>
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div class="form-group form-check" class="w-25 p-3">
+    <input type="checkbox"  class="w-30" class="form-check-input"  id="exampleCheck1"/>
+    <label class="form-check-label" class="w-50 p-10" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
+  <div class="form-group form-check" class="w-25 p-3">
+  <button type="submit" class="w-25 p-10" class="btn btn-primary">Submit</button>
+  </div>
 </form>
 
 
